aee56c4bb02db945399864333481f0a267dbc023
Ashish <ashishkumarmohanty@gmail.com>
2022-05-04 13:31:26 +0530

    commit changes

--- netmeds-clone/src/App.jsx
+++ netmeds-clone/src/App.jsx
@@ -1,16 +1,19 @@
 import './App.css';
 import ProductPage from './components/ProductPage/ProductPage';
-import { Route, Routes } from 'react-router-dom';
+import { Route, Routes, useParams } from 'react-router-dom';
 import CovidRoutes from './components/ProductPage/covidRoutes';
 import Diabeties from './components/ProductPage/diabetiesRoutes';
+import ProductDetails from './components/ProductDetailsPage/ProductDetails';
 
 function App() {
+  
   return (
     <div className="App">
       <ProductPage/>
       <Routes>
         <Route path="/covid" element={<CovidRoutes/>}></Route>
         <Route path="/diabeties" element={<Diabeties/>}></Route>
+        <Route path="/products/:id" element={<ProductDetails/>}></Route>
       </Routes>
     </div>
   );
