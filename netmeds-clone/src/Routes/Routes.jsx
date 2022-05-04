b668ba21945002332c66becd33bc662d3d170dba
Neha Sen <neha712513@gmail.com>
2022-05-04 19:48:23 +0530

    ok

--- netmeds-clone/src/Routes/Routes.jsx
+++ netmeds-clone/src/Routes/Routes.jsx
@@ -3,11 +3,8 @@ import { Route, Routes } from 'react-router-dom';
 import { Cart } from "../components/Cart/Cart";
 import { Navbar, Navbar1, Nav_Menu } from "../components/Nav/Navbar";
 import { ProductDetails } from "../components/ProductDetailsPage/ProductDetails";
-<<<<<<< HEAD
-=======
 import CovidRoutes from "../components/ProductPage/covidRoutes";
 import Diabeties from "../components/ProductPage/diabetiesRoutes";
->>>>>>> 9070f0a012d08fabf47d048841d22c2e4d05e80a
 
 export const AllRoutes = () => {
     return (
@@ -15,18 +12,15 @@ export const AllRoutes = () => {
             <Navbar />
             <Navbar1 />
             <Nav_Menu />
-<<<<<<< HEAD
            
             <Routes>
                 <Route path="/productdetails" element={<ProductDetails />}> </Route>
                 <Route path="/productpage" element={<ProductPage />}> </Route>
                 <Route path="/cart" element={<Cart />}> </Route>
-=======
->>>>>>> 9070f0a012d08fabf47d048841d22c2e4d05e80a
 
 
 
-            <Routes>
+           
                 <Route path="/cart" element={<Cart />}></Route>
                 <Route path="/covid" element={<CovidRoutes />}></Route>
                 <Route path="/diabeties" element={<Diabeties />}></Route>
