be9e16cbd6d2beab93957aa9a049e49d47f1b17a
Neha Sen <neha712513@gmail.com>
2022-05-04 13:58:21 +0530

    resolved routes error

--- netmeds-clone/src/Routes/Routes.jsx
+++ netmeds-clone/src/Routes/Routes.jsx
@@ -1,10 +1,10 @@
-import {ProductPage} from "../components/ProductPage/ProductPage";
+import { ProductPage } from "../components/ProductPage/ProductPage";
 import { Route, Routes } from 'react-router-dom';
-import {CovidRoutes} from '../components/ProductPage/covidRoutes';
-import {Diabeties} from '../components/ProductPage/diabetiesRoutes';
+import { CovidRoutes } from '../components/ProductPage/covidRoutes';
+import { Diabeties } from '../components/ProductPage/diabetiesRoutes';
 import { Navbar } from "../components/Nav/Navbar.jsx";
 import { Navbar1, Nav_Menu } from "../components/Nav/Navbar.jsx";
-import {Cart} from "../components/Cart/Cart";
+import { Cart } from "../components/Cart/Cart";
 
 
 export const AllRoutes = () => {
@@ -13,18 +13,13 @@ export const AllRoutes = () => {
             <Navbar />
             <Navbar1 />
             <Nav_Menu />
-            <ProductPage/>
+            <ProductPage />
             <Routes>
                 <Route path="/covid" element={<CovidRoutes />}></Route>
                 <Route path="/diabeties" element={<Diabeties />}></Route>
-<<<<<<< HEAD
-                <Route path="/cart" element={ <Cart/> }></Route>
-=======
-                <Route path="/products/:id" element={<ProductDetails/>}></Route>
->>>>>>> af78969bb2288b888da75b1db1d33c432bd77f30
+                <Route path="/cart" element={<Cart />}></Route>
             </Routes>
         </>
     )
-
 
 }
