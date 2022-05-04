d6c6646aedfea5fd0d22046c48285767c7a20e9e
Neha Sen <neha712513@gmail.com>
2022-05-04 13:45:41 +0530

    ok

--- netmeds-clone/src/Routes/Routes.jsx
+++ netmeds-clone/src/Routes/Routes.jsx
@@ -4,6 +4,7 @@ import {CovidRoutes} from '../components/ProductPage/covidRoutes';
 import {Diabeties} from '../components/ProductPage/diabetiesRoutes';
 import { Navbar } from "../components/Nav/Navbar.jsx";
 import { Navbar1, Nav_Menu } from "../components/Nav/Navbar.jsx";
+import {Cart} from "../components/Cart/Cart";
 
 
 export const AllRoutes = () => {
@@ -17,6 +18,7 @@ export const AllRoutes = () => {
             <Routes>
                 <Route path="/covid" element={<CovidRoutes />}></Route>
                 <Route path="/diabeties" element={<Diabeties />}></Route>
+                <Route path="/cart" element={ <Cart/> }></Route>
             </Routes>
 
 
